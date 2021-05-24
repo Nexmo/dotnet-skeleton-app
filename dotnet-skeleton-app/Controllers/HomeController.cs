d55049e1d2fa48ada852f0fede667167fd4a07e9
slorello89 <slorello89@gmail.com>
2021-05-24 15:59:42 -0400

    adding heroku button

--- dotnet-skeleton-app/Controllers/HomeController.cs
+++ dotnet-skeleton-app/Controllers/HomeController.cs
@@ -5,21 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using dotnet_skeleton_app.Models;
+using Vonage;
+using Vonage.Request;
+using Vonage.Accounts;
 
 namespace dotnet_skeleton_app.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
-        {
+        {            
+            var VONAGE_API_KEY = Environment.GetEnvironmentVariable("VONAGE_API_KEY") ?? "VONAGE_API_KEY";
+            var VONAGE_API_SECRET = Environment.GetEnvironmentVariable("VONAGE_API_SECRET") ?? "VONAGE_API_SECRET";
+            Console.WriteLine($"API KEY: {VONAGE_API_KEY}");
+            var creds = Credentials.FromApiKeyAndSecret(VONAGE_API_KEY,VONAGE_API_SECRET);
+            var client = new VonageClient(creds);
+
+            var balance = client.AccountClient.GetAccountBalance();
+            ViewBag.balance = balance.Value;
             return View();
         }
 
